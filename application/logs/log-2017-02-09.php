b31892de603176efb8b5b89faac8b5c32e9f0342
prakvirak <prakvirak54@gmail.com>
2017-02-09 13:59:43 +0700

    firt commit

---
+++ application/logs/log-2017-02-09.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2017-02-09 02:56:09 --> Severity: Warning  --> stream_socket_enable_crypto(): SSL operation failed with code 1. OpenSSL Error messages:
+error:14090086:SSL routines:SSL3_GET_SERVER_CERTIFICATE:certificate verify failed C:\wamp64\www\lms\application\third_party\PHPMailer\class.smtp.php 344
