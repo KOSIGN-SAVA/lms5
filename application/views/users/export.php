5e1e1da6794c953c56c645e17ed92d6500da63c5
prakvirak <prakvirak54@gmail.com>
2017-02-20 16:58:49 +0700

    #6162

--- application/views/users/export.php
+++ application/views/users/export.php
@@ -12,25 +12,29 @@ $sheet->setTitle(mb_strimwidth(lang('users_export_title'), 0, 28, "..."));  //Ma
 $sheet->setCellValue('A1', lang('users_export_thead_id'));
 $sheet->setCellValue('B1', lang('users_export_thead_firstname'));
 $sheet->setCellValue('C1', lang('users_export_thead_lastname'));
-$sheet->setCellValue('D1', lang('users_export_thead_email'));
-$sheet->setCellValue('E1', lang('users_export_thead_manager'));
+$sheet->setCellValue('D1', lang('users_export_thead_login'));
+$sheet->setCellValue('E1', lang('users_export_thead_email'));
+$sheet->setCellValue('F1', lang('users_export_thead_manager'));
 
-$sheet->getStyle('A1:E1')->getFont()->setBold(true);
-$sheet->getStyle('A1:E1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
+$sheet->getStyle('A1:F1')->getFont()->setBold(true);
+$sheet->getStyle('A1:F1')->getAlignment()->setHorizontal(PHPExcel_Style_Alignment::HORIZONTAL_CENTER);
 
-$users = $this->users_model->getUsers();
+$users = $this->users_model->getUsersForExportExcel();
+//echo $this->db->last_query();
+//return;
 $line = 2;
 foreach ($users as $user) {
     $sheet->setCellValue('A' . $line, $user['id']);
     $sheet->setCellValue('B' . $line, $user['firstname']);
     $sheet->setCellValue('C' . $line, $user['lastname']);
-    $sheet->setCellValue('D' . $line, $user['email']);
-    $sheet->setCellValue('E' . $line, $user['manager']);
+    $sheet->setCellValue('D' . $line, $user['login']);
+    $sheet->setCellValue('E' . $line, $user['email']);
+    $sheet->setCellValue('F' . $line, $user['manager_lastname'] . " " . $user['manager_firstname']);
     $line++;
 }
 
 //Autofit
-foreach(range('A', 'E') as $colD) {
+foreach(range('A', 'F') as $colD) {
     $sheet->getColumnDimension($colD)->setAutoSize(TRUE);
 }
 
